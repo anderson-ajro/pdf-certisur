698f043ab2b9052dbfe94b69020dab2ed8d9eb39
Anderson <83408814+anderson-ajro@users.noreply.github.com>
2022-01-05 16:45:48 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <PdfCertisur/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import PdfCertisur from './components/PdfCertisur';
+
+export default {
+  name: 'App',
+
+  components: {
+    PdfCertisur,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
