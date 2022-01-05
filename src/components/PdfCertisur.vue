ea96db11ec40244a989cf167b9d80ee855496f4f
Anderson <83408814+anderson-ajro@users.noreply.github.com>
2022-01-05 16:47:26 -0300

    Add files via upload

--- src/components/PdfCertisur.vue
+++ src/components/PdfCertisur.vue
@@ -1,1 +1,36 @@
+<template>
+  <div class="text-center btn">
 
+      <template>
+        <v-btn
+          color="pink"
+        >
+          <a href="https://drive.google.com/file/d/1YmPbC0fO_HwESmRuxtOtaElGACcBNg9d/view?usp=sharing" target="_blank">Ver Pdf</a>
+        </v-btn>
+      </template>
+
+  </div>
+</template>
+    
+<script>
+  export default {
+    data () {
+      return {}
+    },
+  }
+</script>
+
+<style scoped>
+a{
+text-decoration: none;
+color: white !important;
+}
+
+.btn{
+    height: 100%;
+    background-color: #EEE;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
